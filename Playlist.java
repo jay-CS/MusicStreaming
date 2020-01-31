a58d38a39fee680d06e9e146a1734d4446d3fb69
John Him <xjohnhimx@gmail.com>
2020-01-30 17:23:50 -0800

    Update Playlist.java

--- Playlist.java
+++ Playlist.java
@@ -1,4 +1,30 @@
+import java.util.*;
 
 public class Playlist {
+	HashSet<Json> jsons;
+	
+	public Playlist() {
+		jsons = new HashSet();
+	}
+	
+	public Playlist(HashSet<Json> jsons) {
+		this.jsons = jsons;
+	}
+	
+	public void setJsons(HashSet<Json> jsons) {
+		this.jsons = jsons;
+	}
+	
+	public void addJson(Json json) {
+		jsons.add(json);
+	}
+	
+	public void removeJson(Json json) {
+		jsons.remove(json);
+	}
+	
+	public HashSet<Json> getJsons() {
+		return jsons;
+	}
 	
 }
