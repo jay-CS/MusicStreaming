20d45892ed992b77dbab1fd17e430f799f79a622
jay-CS <jonathanbazan90@gmail.com>
2020-02-11 03:30:15 -0800

    Update Main.java

--- java/Model/Main.java
+++ java/Model/Main.java
@@ -29,5 +29,31 @@ public class Main {
             }
          });  
     }
+    
+    void mp3play(String file) {
+        try {
+            // It uses CECS327InputStream as InputStream to play the song 
+             InputStream is = new CECS327InputStream(file);
+             Player mp3player = new Player(is);
+             mp3player.play();
+	     }
+	     catch (JavaLayerException exception) 
+         {
+	       exception.printStackTrace();
+	     }
+         catch (IOException exception)
+         {
+             exception.printStackTrace();
+         }  
+    }
+     
+     
+    public static void main(String[] args) {
+        Integer i;
+        Main player = new Main();
+        player.mp3play("imperial.mp3");
+    }
+ 
+
 }
 
