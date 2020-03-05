cd76ed8e763e15549eae63323eaa27223bf89258
John Him <xjohnhimx@gmail.com>
2020-03-05 03:55:16 -0800

    Update CommunicationModuleServer.java

--- src/main/java/RPCServer/CommunicationModuleServer.java
+++ src/main/java/RPCServer/CommunicationModuleServer.java
@@ -2,6 +2,8 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class CommunicationModuleServer {
 	
     Dispatcher dispatcher = new Dispatcher();
@@ -20,14 +22,21 @@ public class CommunicationModuleServer {
             byte[] buffer = new byte[FRAGMENT_SIZE];
 
             while (true) {
-
+            	
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                //blocks until message arrives
                 socket.receive(request);
                 String message = new String(request.getData()).trim();
                 System.out.println("Received: " + message);
                 String retString = dispatcher.dispatch(message);
                 byte[] msgBytes = (retString).getBytes();
-                DatagramPacket reply = new DatagramPacket(msgBytes, msgBytes.length, request.getAddress(), request.getPort());
+                
+                //get client address and port
+                InetAddress address = request.getAddress();
+                int port = request.getPort();
+                
+                //reply to client
+                DatagramPacket reply = new DatagramPacket(msgBytes, msgBytes.length, address, port);
                 socket.send(reply);
                 buffer = new byte[FRAGMENT_SIZE];
 
