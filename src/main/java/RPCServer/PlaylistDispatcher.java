b468db006f00c9edd7e96d1541ee1e8a127f758b
John Him <xjohnhimx@gmail.com>
2020-03-08 21:09:31 -0700

    Update PlaylistDispatcher.java

--- src/main/java/RPCServer/PlaylistDispatcher.java
+++ src/main/java/RPCServer/PlaylistDispatcher.java
@@ -5,7 +5,9 @@
  */
 package RPCServer;
 
+import Model.Music;
 import Model.Playlist;
+import Model.User;
 import com.google.gson.Gson;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,21 +16,41 @@ import java.io.IOException;
  *
  * @author samantharain
  */
+
 public class PlaylistDispatcher {
-    
-    
-    public String createPlaylist(String playlistName ) throws IOException {
+    private User profiles;
+
+    public PlaylistDispatcher(){
+        profiles = new User();
+    }
+//
+//    public String getSongs(String sessionID, String playlistName, String pageNum) throws FileNotFoundException {
+//        int pageNumber = Integer.parseInt(pageNum);
+//        if (playlistName.equals("")) {
+//            return new Gson().toJson(new Playlist(playlistName,
+//                    MusicDatabase.GetInstance().getSongs(pageNumber-1)));
+//        } else {
+//            return getPage(sessionID, playlistName, pageNumber-1);
+//        }
+//    }
+
+    public String createPlaylist( String playlistName ) throws IOException {
+        profiles.addPlaylist( playlistName);
         //return new Gson().toJson();
-        return null;
     }
 
     public String deletePlaylist(String playlistName ) throws IOException {
+        profiles.removePlaylist(playlistName);
         //return new Gson().toJson();
-        return null;
     }
 
-    public String addSongToPlaylist(  String playlistName, String songId ) throws IOException {
+    public String addSongToPlaylist(String playlistName, Music music ) throws IOException {
+        
+        profiles.addSong(music,playlistName);
         //return new Gson().toJson();
-        return null;
     }
+//    public String getPage( String sessionID, String playlistName, int index ) throws FileNotFoundException {
+//        Playlist playlist = new Playlist(playlistName, profiles.getPage(Integer.parseInt(sessionID), playlistName, index));
+//        return new Gson().toJson(playlist);
+//    }
 }
