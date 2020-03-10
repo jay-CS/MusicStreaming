c00e60eaccfc5ab1364506e083b25a95c7ba08a7
John Him <xjohnhimx@gmail.com>
2020-03-09 20:07:23 -0700

    Update AccountDispatcher.java

--- src/main/java/RPCServer/AccountDispatcher.java
+++ src/main/java/RPCServer/AccountDispatcher.java
@@ -3,13 +3,47 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 package RPCServer;
 
-/**
- *
- * @author samantharain
- */
+import com.google.gson.Gson;
+import Model.Playlist;
+import Model.User;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 public class AccountDispatcher {
-    
-    
+    private User profile;
+
+    public AccountDispatcher() {
+        
+    }
+
+    public String logIn( String username ) throws FileNotFoundException {
+        User account;
+        if(/*user with username exists*/) ){
+            account = new User(username);
+            ArrayList<Playlist> list = /*account with username*/.getPlaylists;
+            account.setPlaylists(list);
+        }else{
+            account = new User();
+        }
+        Gson gson = new Gson();
+        return gson.toJson(account);
+    }
+
+    public String registerAccount( String username ) throws IOException {
+        if(/*user exists*/){
+            Gson gson = new Gson();
+            return gson.toJson( new User() );
+        }else{
+            User account = new User(username);
+            //add account to database
+            Gson gson = new Gson();
+            return gson.toJson( account );
+        }
+    }
 }
